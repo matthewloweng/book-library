536417e6e8981577d31de12c2489aed7ff621a95
Matthew Ng <matthew.j.ng@gmail.com>
2024-03-31 18:26:05 -0700

    setting up server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/mybrary')
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
