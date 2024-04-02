9bffd8fcaf7c2244f73560b332a7bc685dec8875
Matthew Ng <matthew.j.ng@gmail.com>
2024-04-02 00:31:15 -0700

    finished author routes

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
     name: {
@@ -6,5 +7,32 @@ const authorSchema = new mongoose.Schema({
         required: true
     }
 })
+
+// authorSchema.pre('remove', function(next) {
+//     Book.find({ author: this.id }, (err, books) => {
+//         if (err) {
+//             next(err)
+//         } else if (book.length > 0) {
+//             next(new Error('This author has books still located in this library'))
+//         } else {
+//             next()
+//         }
+//     })
+// })
+
+authorSchema.pre("deleteOne", async function (next) {
+    try {
+        const query = this.getFilter();
+        const hasBook = await Book.exists({ author: query._id });
+  
+        if (hasBook) {
+            next(new Error("This author still has books."));
+        } else {
+            next();
+        }
+    } catch (err) {
+        next(err);
+    }
+});
 
 module.exports = mongoose.model('Author', authorSchema)
