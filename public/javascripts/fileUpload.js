b10a8c2e8545aa577e3480f5de5c816189465326
Matthew Ng <matthew.j.ng@gmail.com>
2024-04-02 04:05:24 -0700

    finished styling and last minute touches

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,14 +1,28 @@
-filePond.registerPlugin(
-    filePondPluginImagePreview,
-    filePondPluginImageResize,
-    filePondPluginFileEncode,
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-)
+if (rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') !== '') {
+    ready()
+} else {
+    document.getElementById('main-css').addEventListener('load', ready)
+}
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+function ready() {
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverAspectRatio
+    filePond.registerPlugin(
+        filePondPluginImagePreview,
+        filePondPluginImageResize,
+        filePondPluginFileEncode,
+    
+    )
+    
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+    
+    FilePond.parse(document.body)
+}
 
-FilePond.parse(document.body)
